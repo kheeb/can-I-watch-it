dd21b2b108399d5e634780f8fed70cc10da20565
DanielWestiner <daniel.westiner@gmail.com>
2021-12-31 14:52:32 -0500

    added functionality to check the value of service selected in user form and updated html form select, changed the order of the process on the main webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,89 @@
 var Kara = "9bc319ba34mshdb0c70c64984539p15f5efjsn8aa923a6ee36";
 var Giovanna = "87d84436d1mshb6b0d209ecb39b5p1ec3d4jsn7e6c7183a483";
 var Daniel = "343b570cfbmsh84ada70a77fa889p16e201jsn1f05562634fe";
-var Troy = "";
+var Troy = "b1f2194ce2msh8465b3fd1005b7ap1448f4jsn54d8bc2cedb5"
 
 var searchBar;
 
-var service = "netflix";
+var service =  document.querySelector('#provider').value;
 
-var movieCard = $("#movieInfoResults");
+
+console.log(service)
+
+var movieCard = $("#movieInfoResults")
+
 
 // Assigning IDs to variables
 
-let movieResult = $("#movie-result");
+// let movieResult = $('#movie-result');
 
-let searchTitle = $("#searchBar");
+let searchTitle = $('#searchBar');
 
-let searchBtn = $("#searchBtn");
+let searchBtn = $('#searchBtn');
+
 
 // Function to create card container which will populate the results from search using the API
 
-function getInfo(title, synopsis) {
-  let cardContainer = $("<div>").addClass("movie-card");
 
-  let movieInfo = $("<div>").addClass("movie-info");
+function getInfo (title, synopsis) {
+    movieCard.empty()
+  let cardContainer = $('<div>').addClass('movie-card');
 
-  let movieTitle = $("<h3>").addClass("movie-title").text(title);
+  let movieInfo = $('<div>').addClass('movie-info');
 
-  movieInfo.append(movieTitle);
+  let movieTitle = $('<h3>').addClass('movie-title').text(title);
 
-  let movieSummary = $("<div>").addClass("movie-summary");
+//   movieInfo.append(movieTitle);
 
-  let movieSynopsis = $("<p>").addClass("movie-synopsis").text(synopsis);
+  let movieSummary = $('<div>').addClass('movie-summary');
 
-  //   let movieImg = $('<img>').addClass('card-img').attr("src", poster);
+  let movieSynopsis = $('<p>').addClass('movie-synopsis').text(synopsis);
 
-  //   movieSummary.append(movieImg);
-  movieSummary.append(movieSynopsis);
-  movieTitle.append(movieSummary);
-  movieInfo.append(movieTitle);
+//   let movieImg = $('<img>').addClass('card-img').attr("src", poster);
+
+//   movieSummary.append(movieImg);
+//   movieSummary.append(movieSynopsis);
+//   movieTitle.append(movieSummary);
+  movieInfo.append(movieTitle).append(movieSynopsis);
   cardContainer.append(movieInfo);
-  movieResult.append(cardContainer);
-  movieCard.append(movieResult);
+  movieCard.append(cardContainer);
+//   movieResult.appendTo(movieCard);
+
 }
 
 // $(document).ready(function () {
-$("#searchBtn").click(function (event) {
-  event.preventDefault();
-  searchBar = $("#searchBar").val();
-  console.log(searchBar);
-  // settings.url = "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" +
-  // service +
-  // "&type=movie&page=1&title=" +
-  // searchBar+
-  // "&output_language=en&language=en"
-  $.ajax({
-    async: true,
-    crossDomain: true,
-    currentSearch: "",
-    url:
-      "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" +
-      service +
-      "&type=movie&page=1&keyword=" +
-      searchBar +
-      "&output_language=en&language=en",
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-      "x-rapidapi-key": "343b570cfbmsh84ada70a77fa889p16e201jsn1f05562634fe",
-    },
-  }).done(function (response) {
-    var searchResults = JSON.parse(response);
-    console.log(searchResults.results);
-    getInfo(searchResults.results.title, searchResults.results.overview);
+  $("#searchBtn").click(function (event) {
+      event.preventDefault();
+      searchBar = $("#searchBar").val()
+        console.log(searchBar)
+        // settings.url = "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" +
+        // service +
+        // "&type=movie&page=1&title=" +
+        // searchBar+
+        // "&output_language=en&language=en"
+    $.ajax({
+        async: true,
+        crossDomain: true,
+        currentSearch: "",
+        url:
+          "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" +
+          document.querySelector('#provider').value +
+          "&type=movie&page=1&keyword="+
+          searchBar+
+          "&output_language=en&language=en",
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+          "x-rapidapi-key": "343b570cfbmsh84ada70a77fa889p16e201jsn1f05562634fe",
+        },
+      }).done(function (response) {
+      var searchResults = JSON.parse(response);
+      console.log(searchResults.results);
+      getInfo(searchResults.results[0].title, searchResults.results[0].overview)
+    });
   });
-});
 // });
+
+ 
+
+document.querySelector('#provider').value
